79fd5062b5de92cb9feb36b15e99b50dc20c4d88
kieranmack <kieranmcmenaman2@gmail.com>
2026-03-03 14:47:22 -0500

    Updated user.java
    added foreign key constraint to User class, to allow access to user role, for role conditional logic.

--- src/main/java/autosearch/proj/application/Entities/User.java
+++ src/main/java/autosearch/proj/application/Entities/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -17,6 +19,10 @@ public class User {
 	private String username;
 	private String password;
 	private String email;
+	
+	@ManyToOne
+	@JoinColumn(name = "roles_id")
+	private Roles role;
 	
 	public int getId() {
 		return userId;
