a70f8a162f534dccd7450c02acb984a2ad49b05f
kieranmack <kieranmcmenaman2@gmail.com>
2026-03-09 16:04:49 -0400

    Scraping Test Format
    Cleaned up Scrape test class to be able to showcase better in class presentation.

--- src/test/java/autosearch/proj/application/TestBasicScrapingLogic.java
+++ src/test/java/autosearch/proj/application/TestBasicScrapingLogic.java
@@ -33,14 +33,21 @@ public class TestBasicScrapingLogic {
 
 		Document doc = Jsoup.parse(file, "UTF-8");
 
-		// We now have content, select the heading, then print
+		// We now have content in the form of a doc obj, 
+		//select the heading's text, then print
 		String headerText = doc.selectFirst("h2").text();
-		System.out.println("Header: " + headerText);
-
+		System.out.println("Header Text: " + headerText);
+		System.out.println("///////////////////////////////\n");
+		
+		
 		// testing to grab div using selectFirst tab
 		String firstListing = doc.selectFirst("div").text();
 		System.out.println("First listing (test): " + firstListing);
+		System.out.println("///////////////////////////////\n");
 
+		String firstListingHTML = doc.selectFirst("div").html();
+		System.out.println("First Listing HTML: " + firstListingHTML);
+		System.out.println("///////////////////////////////\n");
 		// Now we will test to see if we can use for each loop using Jsoups element
 		// object
 		// and succesfully check through each div.
@@ -49,40 +56,49 @@ public class TestBasicScrapingLogic {
 		// Elements pulls everything that is annotated with it's respective tag, "p" or
 		// "div"
 		// in this case
-		Elements paragraph = doc.select("p");
+		Elements paragraphs = doc.select("p");
 
-		for (Element para : paragraph) {
+		for (Element para : paragraphs) {
 			System.out.println("Paragraphs: " + para.text());
 		}
+		System.out.println("///////////////////////////////\n");
 
 		// success, nice all there needs to be done is to create static sites, and then
 		// populate raw car objects
 		for (Element div : divs) {
 			System.out.println("Div: " + div.text());
 		}
+		System.out.println("///////////////////////////////\n");
 
 		// testing to pull out data from each span class in HTML file
 		Element testCarDiv = doc.selectFirst(".car-listing");
 		System.out.println(testCarDiv);
+		System.out.println("///////////////////////////////\n");
 		
 		//Selecting each value using the div tag, ."value" to indicate what to pull from
-		String year = testCarDiv.selectFirst(".year").text();
-		String make = testCarDiv.selectFirst(".make").text();
-		String model = testCarDiv.selectFirst(".model").text();
+		String year = testCarDiv.selectFirst(".year").text()
+				.replace("Year: ", "");
+		
+		String make = testCarDiv.selectFirst(".make").text()
+				.replace("Make: ", "");
+		
+		String model = testCarDiv.selectFirst(".model").text()
+			.replace("Model: " , "" );
+		
 		String mileageText = testCarDiv.selectFirst(".mileage").text()
 				.replace("Mileage: ", "");
 		int mileage = Integer.parseInt(mileageText);
 		
 		String priceText = testCarDiv.selectFirst(".price").text()
 				.replace("Price: ", "");
-			
-		
 		double price = Double.parseDouble(priceText);
 		
 		Car car = new Car(make, model, year, mileage, price, source, currentDate);
 		
 		System.out.println("Cars Attributes: " + make + "\n" + model + "\n"  + year + "\n" 
 						+ mileage + "\n" + price + "\n" + source + "\n"  + currentDate );
+		
+		System.out.println("///////////////////////////////\n");
 		
 		//I will now duplicate it exactly, but try to loop through to populate all listings 
 		//on the HTML
@@ -107,6 +123,8 @@ public class TestBasicScrapingLogic {
 			
 			System.out.println("Cars Attributes: " + make1 + "\n" + model1 + "\n"  + year1 + "\n" 
 							+ mileage1 + "\n" + price1 + "\n" + source + "\n"  + currentDate );
+			
+			System.out.println("///////////////////////////////\n");
 			
 		}
 		//nevermind it's working, just had to fix formatting in my div elements, now all 
