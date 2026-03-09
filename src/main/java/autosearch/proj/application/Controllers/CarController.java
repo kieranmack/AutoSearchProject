f9061581b002389383c71d9ee8098c2693760771
kieranmack <kieranmcmenaman2@gmail.com>
2026-03-09 16:04:15 -0400

    Controller Endpoints
    Removed POST endpoint, as I don't see it ever really being used in my code.  Better to show only GET to show basic core functionality.

--- src/main/java/autosearch/proj/application/Controllers/CarController.java
+++ src/main/java/autosearch/proj/application/Controllers/CarController.java
@@ -15,28 +15,24 @@ import autosearch.proj.application.Services.CarService;
 @RestController
 public class CarController {
 
-	//CarService object
+	// CarService object
 	private final CarService carService;
-	
-	//constructor to create carservice object to make calls to the service class
+
+	// constructor to create carservice object to make calls to the service class
 	public CarController(CarService carService) {
 		this.carService = carService;
 	}
-	
-	//Endpoint for GET HTTP method, ie READ (CRUD), this will return all car objects in
-	//the database as JSON
-	@GetMapping("/carsearch")
-	 @ResponseBody
-	    public List<Car> getAllCars() {
-	        return carService.returnAllCars();
 
-	 }
-	
-	//Testing Post endpoint, but since my front end will really be read only
-	//I think I will only need GET endpoints, will leave it for now PROBABLY DELETE LATER
-	@PostMapping("/carsearch")
+	// Endpoint for GET HTTP method, ie READ (CRUD), this will return all car
+	// objects in
+	// the database as JSON
+	@GetMapping("/carsearch")
 	@ResponseBody
-	public Car createCar(@RequestBody Car car) {
-		return carService.createCar(car);
+	public List<Car> getAllCars() {
+		return carService.returnAllCars();
+
 	}
-}
+
+	
+	}
+
