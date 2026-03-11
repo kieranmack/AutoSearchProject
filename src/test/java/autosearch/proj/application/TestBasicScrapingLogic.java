686324b1b413ac3ca1dadc8e284d449a61b713f0
kieranmack <kieranmcmenaman2@gmail.com>
2026-03-11 17:45:08 -0400

    Scrape Logic Testing Complete
    Have a good handle on the scraping logic when the HTML is this simple.  It has already been implemented into the project and works as intended.  Deleted some stuff from the test class because it was getting out of control.

--- src/test/java/autosearch/proj/application/TestBasicScrapingLogic.java
+++ src/test/java/autosearch/proj/application/TestBasicScrapingLogic.java
@@ -5,19 +5,39 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
 
+import autosearch.proj.application.DTOs.CarDTO;
 import autosearch.proj.application.Entities.Car;
+import autosearch.proj.application.Repositories.CarRepository;
+import autosearch.proj.application.Services.CarService;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
+@SpringBootTest
 public class TestBasicScrapingLogic {
+
+	@Autowired
+	private CarRepository carRepo;
+
+	@Autowired
+	private CarService carService;
+	// Annotations allow me to test with everything in my main project in a test
+	// class
 
 	@Test
 	public void testScrape() throws IOException {
-		
+
+		List<Car> checkList = new ArrayList<>();
+		List<Car> dbList = new ArrayList<>();
+		dbList = carRepo.findAll();
+
 		Date currentDate = new Date();
 		String source = "siteA.html";
 		// File object which reads the classpath and grabs the matching file
@@ -32,13 +52,12 @@ public class TestBasicScrapingLogic {
 
 		Document doc = Jsoup.parse(file, "UTF-8");
 
-		// We now have content in the form of a doc obj, 
-		//select the heading's text, then print
+		// We now have content in the form of a doc obj,
+		// select the heading's text, then print
 		String headerText = doc.selectFirst("h2").text();
 		System.out.println("Header Text: " + headerText);
 		System.out.println("///////////////////////////////\n");
-		
-		
+
 		// testing to grab div using selectFirst tab
 		String firstListing = doc.selectFirst("div").text();
 		System.out.println("First listing (test): " + firstListing);
@@ -51,7 +70,7 @@ public class TestBasicScrapingLogic {
 		// object
 		// and succesfully check through each div.
 
-		Elements divs = doc.select("div"); 
+		Elements divs = doc.select("div");
 		// Elements pulls everything that is annotated with it's respective tag, "p" or
 		// "div"
 		// in this case
@@ -73,61 +92,57 @@ public class TestBasicScrapingLogic {
 		Element testCarDiv = doc.selectFirst(".car-listing");
 		System.out.println(testCarDiv);
 		System.out.println("///////////////////////////////\n");
-		
-		//Selecting each value using the div tag, ."value" to indicate what to pull from
-		String year = testCarDiv.selectFirst(".year").text()
-				.replace("Year: ", "");
-		
-		String make = testCarDiv.selectFirst(".make").text()
-				.replace("Make: ", "");
-		
-		String model = testCarDiv.selectFirst(".model").text()
-			.replace("Model: " , "" );
-		
-		String mileageText = testCarDiv.selectFirst(".mileage").text()
-				.replace("Mileage: ", "");
+
+		// Selecting each value using the div tag, ."value" to indicate what to pull
+		// from
+		String year = testCarDiv.selectFirst(".year").text().replace("Year: ", "");
+
+		String make = testCarDiv.selectFirst(".make").text().replace("Make: ", "");
+
+		String model = testCarDiv.selectFirst(".model").text().replace("Model: ", "");
+
+		String mileageText = testCarDiv.selectFirst(".mileage").text().replace("Mileage: ", "");
 		int mileage = Integer.parseInt(mileageText);
-		
-		String priceText = testCarDiv.selectFirst(".price").text()
-				.replace("Price: ", "");
+
+		String priceText = testCarDiv.selectFirst(".price").text().replace("Price: ", "");
 		double price = Double.parseDouble(priceText);
-		
+
 		Car car = new Car(make, model, year, mileage, price, source, currentDate);
-		
-		System.out.println("Cars Attributes: " + make + "\n" + model + "\n"  + year + "\n" 
-						+ mileage + "\n" + price + "\n" + source + "\n"  + currentDate );
-		
+
+		System.out.println("Cars Attributes: " + make + "\n" + model + "\n" + year + "\n" + mileage + "\n" + price
+				+ "\n" + source + "\n" + currentDate);
+
 		System.out.println("///////////////////////////////\n");
-		
-		//I will now duplicate it exactly, but try to loop through to populate all listings 
-		//on the HTML
+
+		// I will now duplicate it exactly, but try to loop through to populate all
+		// listings
+		// on the HTML
 		System.out.println("THIS IS WHERE I AM LOOPING THROUGH EVERY ELEMENT\n");
 		Elements carListings = doc.select(".car-listing");
-		
-		for(Element listing : carListings) {
+
+		for (Element listing : carListings) {
 			String year1 = listing.selectFirst(".year").text();
 			String make1 = listing.selectFirst(".make").text();
 			String model1 = listing.selectFirst(".model").text();
-			String mileageText1 = listing.selectFirst(".mileage").text()
-					.replace("Mileage: ", "");
+			String mileageText1 = listing.selectFirst(".mileage").text().replace("Mileage: ", "");
 			int mileage1 = Integer.parseInt(mileageText1);
-			
-			String priceText1 = listing.select(".price").text()
-					.replace("Price: ", "");
-				
-			
-			double price1 = Double.parseDouble(priceText1);
-			
-			Car car1 = new Car(make1, model1, year1, mileage1, price1, source, currentDate);
-			
-			System.out.println("Cars Attributes: " + make1 + "\n" + model1 + "\n"  + year1 + "\n" 
-							+ mileage1 + "\n" + price1 + "\n" + source + "\n"  + currentDate );
-			
-			System.out.println("///////////////////////////////\n");
-			
-		}
-		//nevermind it's working, just had to fix formatting in my div elements, now all 
-		//I really need to do is implement this logic saving to the database, way ahead of schedule. 
 
+			String priceText1 = listing.select(".price").text().replace("Price: ", "");
+
+			double price1 = Double.parseDouble(priceText1);
+
+			Car car1 = new Car(make1, model1, year1, mileage1, price1, source, currentDate);
+
+			System.out.println("Cars Attributes: " + make1 + "\n" + model1 + "\n" + year1 + "\n" + mileage1 + "\n"
+					+ price1 + "\n" + source + "\n" + currentDate);
+
+			System.out.println("///////////////////////////////\n");
+			System.out.println("///////////////////////////////\n");
+			System.out.println("///////////////////////////////\n");
+
+			//Basic scrape logic complete, will create new test file to test duplicate. 
+
+		}
 	}
 }
+
