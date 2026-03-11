28070bd820aab43a7a577d1a050823c027a233bc
kieranmack <kieranmcmenaman2@gmail.com>
2026-03-10 23:44:31 -0400

    Controller and Scrape Service Updates
    Added Admin Controller endpoint.  Need to add scheduled endpoint.  Also scrape service working as intended, and correctly saving entities to database.  Will add duplicate logic next.

---
+++ src/main/java/autosearch/proj/application/Controllers/AdminController.java
@@ -0,0 +1,33 @@
+package autosearch.proj.application.Controllers;
+
+import java.io.IOException;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import autosearch.proj.application.Services.ScraperServiceImpl;
+
+@RestController
+public class AdminController {
+	
+	//scraper service object
+	private final ScraperServiceImpl scraperServ;
+	
+	//admincontroller constructor
+	public AdminController(ScraperServiceImpl scraperServ) {
+		this.scraperServ = scraperServ;
+	}
+	//endpoint for triggering scrape manually, will add scheduled later on
+		@GetMapping("/adminscrape")
+			public String triggerScrape() {
+			try {
+			scraperServ.scrapeAndSaveSiteA();
+			return "Scrape Success!";
+		}catch (IOException e) {
+			e.printStackTrace();
+			return "Time to debug" +  e.getMessage();
+		}
+			
+		
+		}
+}
--- src/main/java/autosearch/proj/application/Services/ScraperServiceImpl.java
+++ src/main/java/autosearch/proj/application/Services/ScraperServiceImpl.java
@@ -1,5 +1,69 @@
 package autosearch.proj.application.Services;
 
-public class ScraperServiceImpl implements ScraperService {
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import java.util.List;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+
+import autosearch.proj.application.Entities.Car;
+import autosearch.proj.application.Repositories.CarRepository;
+
+@Service
+public class ScraperServiceImpl {
+	//car repo object for making calls to repository
+	private final CarRepository carRepo;
+	
+	//constructor to create car Repo object
+	ScraperServiceImpl(CarRepository carRepo){
+		this.carRepo = carRepo;
+	}
+
+	public void scrapeAndSaveSiteA() throws IOException {
+		
+		Date currentDate = new Date();
+		String source = "siteA.html";
+		// File object which reads the classpath and grabs the matching file
+		File file = new ClassPathResource("static/siteA.html").getFile();
+
+		// check if file exists, if doesn;t break the logic so as to not break anything
+		
+		Document doc = Jsoup.parse(file, "UTF-8");
+		
+		//create list of elements
+		Elements carListings = doc.select(".car-listing");
+		
+		//loop through each element to extract their data. 
+		for(Element listing : carListings) {
+			String year = listing.selectFirst(".year").text()
+					.replace("Year: ", "");
+			
+			String make = listing.selectFirst(".make").text()
+					.replace("Make: ", "");
+			
+			String model = listing.selectFirst(".model").text()
+				.replace("Model: " , "" );
+			
+			String mileageText = listing.selectFirst(".mileage").text()
+					.replace("Mileage: ", "");
+			int mileage = Integer.parseInt(mileageText);
+			
+			String priceText = listing.selectFirst(".price").text()
+					.replace("Price: ", "");
+			double price = Double.parseDouble(priceText);
+			
+			//create new car object using attributes
+			Car car = new Car(make, model, year, mileage, price, source, currentDate);
+			
+			carRepo.save(car);
+			
+		}
+		
+	}
 }
