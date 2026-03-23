d618e79b5c95a57efa8ade93ed604756f5d74e37
kieranmack <kieranmcmenaman2@gmail.com>
2026-03-23 14:22:48 -0400

    Updated UserRepo
    Added find by Username and Password method, for eventual login methods.

--- src/main/java/autosearch/proj/application/Repositories/UserRepository.java
+++ src/main/java/autosearch/proj/application/Repositories/UserRepository.java
@@ -1,9 +1,12 @@
 package autosearch.proj.application.Repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import autosearch.proj.application.Entities.User;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
+	 User findByUsernameAndPassword(String username, String password);
 }
