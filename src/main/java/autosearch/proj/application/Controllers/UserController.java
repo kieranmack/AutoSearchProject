e29017eddb272290f6ae5ee996676ee9dd7e914d
kieranmack <kieranmcmenaman2@gmail.com>
2026-03-24 18:33:08 -0400

    Added User Controller Endps.
    Endpoints for register and login, request parameter annotation required to bind HTTP request parameters to one that spring can read. These call Service methods.

--- src/main/java/autosearch/proj/application/Controllers/UserController.java
+++ src/main/java/autosearch/proj/application/Controllers/UserController.java
@@ -3,11 +3,14 @@ package autosearch.proj.application.Controllers;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import autosearch.proj.application.Entities.User;
 import autosearch.proj.application.Services.UserService;
+import autosearch.proj.application.Services.UserServiceImpl;
 
 //Rest API Annotation
 
@@ -25,5 +28,22 @@ public class UserController {
 	public List<User> returnAllUsers() {
 		
 		return userService.returnAllUsers();
+	}
+	
+	@PostMapping("/user/register/")
+	@ResponseBody
+	public String registerUser(
+			@RequestParam String username,
+			@RequestParam String password,
+			@RequestParam String email) {
+		return userService.registerUser(username, password, email);
+	}
+	
+	@PostMapping("/user/login/")
+	@ResponseBody
+	public String loginUser(
+			@RequestParam String username,
+			@RequestParam String password) {
+		return userService.loginUser(username, password);
 	}
 }
