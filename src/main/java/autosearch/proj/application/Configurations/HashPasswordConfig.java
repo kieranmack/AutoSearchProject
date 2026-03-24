73eea18969f3a3f3df5d3221c73a8ad905b23dd5
kieranmack <kieranmcmenaman2@gmail.com>
2026-03-24 18:30:18 -0400

    Update HashPasswordConfig.java

--- src/main/java/autosearch/proj/application/Configurations/HashPasswordConfig.java
+++ src/main/java/autosearch/proj/application/Configurations/HashPasswordConfig.java
@@ -21,6 +21,7 @@ public class HashPasswordConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+            .csrf(csrf -> csrf.disable()) 
             .authorizeHttpRequests(auth -> auth
                 .anyRequest().permitAll() 
             );
