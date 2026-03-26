03b31912d7c0eb7aa88c23558aeed55045dc9cd3
kieranmack <kieranmcmenaman2@gmail.com>
2026-03-26 12:20:52 -0400

    Updated User and Car Repo
    Added extension of JpaSpecificationExecutor, used to build dynamic queries based upon specified parameters, included or not, more in service.

--- src/main/java/autosearch/proj/application/Repositories/CarRepository.java
+++ src/main/java/autosearch/proj/application/Repositories/CarRepository.java
@@ -1,6 +1,10 @@
 package autosearch.proj.application.Repositories;
 
+import java.util.List;
+
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import autosearch.proj.application.Entities.Car;
@@ -8,7 +12,10 @@ import autosearch.proj.application.Entities.Car;
 //Repository, will add own methods later, but JpaRepository is incredibly strong, supplying 
 //methods with strong abstractions making it possible to map to any database regardless of content
 
+@Repository
+public interface CarRepository extends JpaRepository<Car, Integer>,
+	JpaSpecificationExecutor<Car> {
 
-public interface CarRepository extends JpaRepository<Car, Integer> {
-
+	List<Car> findAll(Specification<Car> spec);
+	
 }
--- src/main/java/autosearch/proj/application/Repositories/UserRepository.java
+++ src/main/java/autosearch/proj/application/Repositories/UserRepository.java
@@ -1,22 +1,24 @@
 package autosearch.proj.application.Repositories;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import autosearch.proj.application.Entities.User;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer>, 
+	JpaSpecificationExecutor<User>{
 
 	 User findByUsernameAndPassword(String username, String password);
 	 
 	 User findByUsername(String username);
 	 
 	
-	 
 	 User findById(int id);
 	 
-	 User findByPassword(String password);
-	 
+	 List<User> findAll(Specification<User> spec);
 	 
 }
