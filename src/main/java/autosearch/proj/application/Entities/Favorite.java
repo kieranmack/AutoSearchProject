dc179ff7e0460b29acd2278f97b5d69e21ae07ec
kieranmack <kieranmcmenaman2@gmail.com>
2026-03-30 08:43:20 -0400

    Created Favorites Entity
    Favorites Entity created, will begin coding favorite logic.  Need a way to save user data for logins when making a favorite request.

---
+++ src/main/java/autosearch/proj/application/Entities/Favorite.java
@@ -0,0 +1,29 @@
+package autosearch.proj.application.Entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "favorites")
+public class Favorite {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "favorite_id")
+	private int favoriteId;
+	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+	
+	@ManyToOne
+	@JoinColumn(name = "cars_id")
+	private Car car;
+
+}
