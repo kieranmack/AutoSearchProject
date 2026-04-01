bc9cc54b5d9ae21a3b8253d5bbf85d8c6653ef2e
kieranmack <kieranmcmenaman2@gmail.com>
2026-04-01 13:52:16 -0400

    Javascript for login and Register
    Javascript, created to deal with login and Register, redirects properly, displays new buttons and reading backend return types properly, once styled these pages wil be complete.

---
+++ src/main/resources/static/loginscript.js
@@ -0,0 +1,40 @@
+
+
+//Javascript File for login User
+	document.getElementById('loginForm').addEventListener('submit', async function(event) {
+   	 	event.preventDefault(); 
+		
+		const username = document.getElementById('username').value;
+		const password = document.getElementById('password').value;
+		
+		const formData = new URLSearchParams();
+		
+		formData.append('username', username);
+		formData.append('password', password);
+		
+		try {
+				//await method, connects to controller endpoint
+		        const response = await fetch('user/login/', {
+		            method: 'POST',
+					//content in the form of URL query params, not JSOn 
+		            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+		            body: formData
+		        });
+				
+		const result = await response.json();
+		
+		
+			 
+		if(!result.success){
+			 document.getElementById('message').textContent = result.message;
+		}else if(result.message == "Admin"){
+			window.location.href = "http://localhost:8080/admindashboard.html";
+		}else
+			window.location.href = "http://localhost:8080/search.html";
+			
+		
+			 } catch (error) {
+			         console.error('Error:', error);
+			         document.getElementById('message').textContent = 'Something went wrong';
+			     }
+			 });	 
---
+++ src/main/resources/static/registerscript.js
@@ -0,0 +1,43 @@
+
+
+//Javascript File for Registering User
+	document.getElementById('registerForm').addEventListener('submit', async function(event) {
+   	 	event.preventDefault(); 
+
+	//get doc values, then append on the URL as a url search parameter
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const email = document.getElementById('email').value;
+	const formData = new URLSearchParams();
+	formData.append('username', username);
+	formData.append('password', password);
+	formData.append('email', email);
+
+    try {
+		//await method, connects to controller endpoint
+        const response = await fetch('user/register/', {
+            method: 'POST',
+			//content in the form of URL query params, not JSOn 
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: formData
+        });
+
+		//Spring auto returns as Json, so return our custom Object as Json, then display message
+        const result = await response.json();
+        document.getElementById('message').textContent = result.message;
+		
+		//if the success attribute is true, allow loginButton to show. 
+		if(result.success == true){
+			document.getElementById('loginButton').style.display = "block";
+		}
+		
+		const button = document.getElementById('loginButton').addEventListener('click', function(){
+			window.location.href = "http://localhost:8080/login.html";
+		});
+	
+		
+    } catch (error) {
+        console.error('Error:', error);
+        document.getElementById('message').textContent = 'Something went wrong';
+    }
+});
