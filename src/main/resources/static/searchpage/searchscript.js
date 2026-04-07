add0ef78570bf4a2f8cb41cd3aeddeb6849991bb
kieranmack <kieranmcmenaman2@gmail.com>
2026-04-07 10:58:46 -0400

    Adding Functionality to Search
    Added year range, works as intended.  Setting and Deleting URL parameters.

--- src/main/resources/static/searchpage/searchscript.js
+++ src/main/resources/static/searchpage/searchscript.js
@@ -3,42 +3,86 @@
 //basic
 
 const url = new URL('http://localhost:8080/api/search/?');
+const favoriteUrl = new URL('http://localhost:8080/api/search/favorites/')
 const searchButton = document.getElementById('searchButton');
+const favButton = document.getElementById('favoritesButton');
 
+//dropdowns, if changed value, append URL search.
+
+//need to make it so they delete the search if going back to default value. 
 const makeDropdown = document.getElementById('makeSelect');
 
-	makeDropdown.addEventListener('change', function() {
+makeDropdown.addEventListener('change', function() {
 
-    	const selectedValue = makeDropdown.value;
-    	url.searchParams.set('make', selectedValue);
-    	console.log(url);
+    const selectedValue = makeDropdown.value;
+
+    if (selectedValue == "default") {
+        url.searchParams.delete('make');
+    } else
+
+        url.searchParams.set('make', selectedValue);
+    console.log(url);
 
 });
 
 const modelDropdown = document.getElementById('modelSelect');
 
-	modelDropdown.addEventListener('change', function(){
-		const selectedValue = modelDropdown.value;
-		url.searchParams.set('model', selectedValue);
-		console.log(url);
-		
+modelDropdown.addEventListener('change', function() {
+    const selectedValue = modelDropdown.value;
+
+    if (selectedValue == "default") {
+        url.searchParams.delete('model')
+    } else
+
+        url.searchParams.set('model', selectedValue);
+    console.log(url);
+
 });
 
-searchButton.addEventListener('click', function(){
-	fetch(url)
-	    .then(response => response.json()) // converts raw JSON to JS object/array
-	    .then(data => {
+const yearDropdown = document.getElementById('yearSelect');
 
-	        const returnVals = document.getElementById('message');
+yearDropdown.addEventListener('change', function() {
 
 
-	        returnVals.textContent = JSON.stringify(data, null, 2);
-	        console.log(data); 
-	    })
-	    .catch(err => console.error(err));
-	
+    const [minYear, maxYear] = yearDropdown.value.split("-");
+    if (yearDropdown.value == "default") {
+        url.searchParams.delete('minYear');
+        url.searchParams.delete('maxYear');
+    } else
+
+        url.searchParams.set('minYear', minYear);
+    url.searchParams.set('maxYear', maxYear);
+    console.log(url);
+
+});
+
+searchButton.addEventListener('click', function() {
+    fetch(url)
+        .then(response => response.json()) // converts raw JSON to JS object/array
+        .then(data => {
+
+            const returnVals = document.getElementById('message');
+
+
+            returnVals.textContent = JSON.stringify(data, null, 2);
+            console.log(data);
+        })
+        .catch(err => console.error(err));
+
+});
+
+favButton.addEventListener('click', function() {
+
+    fetch(favoriteUrl)
+        .then(response => response.json())
+        .then(data => {
+            const returnVals = document.getElementById('message');
+
+            returnVals.textContent = JSON.stringify(data, null, 2);
+        })
+        .catch(err => console.error(err));
+
 });
 
 
 
-    
