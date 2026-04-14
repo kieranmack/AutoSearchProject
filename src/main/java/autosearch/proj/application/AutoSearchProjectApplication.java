42644c14c1bdde013b28419370ac4450b09fb821
kieranmack <kieranmcmenaman2@gmail.com>
2026-04-14 15:24:10 -0400

    Added Scheduled To Application
    Added enable scheduling to application class, which allows for scheduling across entire projec.t

--- src/main/java/autosearch/proj/application/AutoSearchProjectApplication.java
+++ src/main/java/autosearch/proj/application/AutoSearchProjectApplication.java
@@ -2,9 +2,11 @@ package autosearch.proj.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 //Main class that runs the entire project, nothing will change here. 
 @SpringBootApplication
+@EnableScheduling
 public class AutoSearchProjectApplication {
 
 	public static void main(String[] args) {
