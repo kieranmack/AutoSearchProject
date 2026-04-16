0c3fd75619fb363e4046adbbc55bb94ba6f02ad2
kieranmack <kieranmcmenaman2@gmail.com>
2026-04-16 16:01:35 -0400

    Admin Controller
    Return stats, and cars as well as refactor scrape to return scrape summary DTO.

--- src/main/java/autosearch/proj/application/Controllers/AdminController.java
+++ src/main/java/autosearch/proj/application/Controllers/AdminController.java
@@ -3,31 +3,58 @@ package autosearch.proj.application.Controllers;
 import java.io.IOException;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import autosearch.proj.application.DTOs.AdminStatsDTO;
 import autosearch.proj.application.DTOs.CarDTO;
+import autosearch.proj.application.DTOs.ScrapeSummaryDTO;
+import autosearch.proj.application.Services.AdminService;
+import autosearch.proj.application.Services.CarService;
 import autosearch.proj.application.Services.ScraperServiceImpl;
 
 @RestController
 public class AdminController {
-	
-	//scraper service object
-	private final ScraperServiceImpl scraperServ;
-	
-	//admincontroller constructor
+
+	// scraper service object
+	@Autowired
+	private ScraperServiceImpl scraperServ;
+
+	@Autowired
+	private CarService carService;
+
+	@Autowired
+	private AdminService adminService;
+
+	// admincontroller constructor
 	public AdminController(ScraperServiceImpl scraperServ) {
 		this.scraperServ = scraperServ;
 	}
-	//endpoint for triggering scrape manually, will add scheduled later on
-	
-	
-		@GetMapping("/adminscrape")
-			public List<CarDTO> triggerScrape() throws IOException {
-			
-			return scraperServ.scrapeAndSaveSite();
-			
-		
-		}
-		
+	// endpoint for triggering scrape manually, will add scheduled later on
+
+	@GetMapping("/adminscrape")
+	public ScrapeSummaryDTO triggerScrape() throws IOException {
+
+		return scraperServ.scrapeAndSaveSite();
+
+	}
+
+	//returns 10 most recent cars
+	@GetMapping("/admin/getcars")
+	@ResponseBody
+	public List<CarDTO> getCars() {
+
+		return carService.findAdminCars();
+	}
+
+	// controller for returning stats
+	@GetMapping("/admin/stats")
+	@ResponseBody
+	public AdminStatsDTO getStats() {
+		return adminService.returnStats();
+	}
+
 }
