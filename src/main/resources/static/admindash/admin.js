445d65762396531b4d779b36957c45091bfa33ca
kieranmack <kieranmcmenaman2@gmail.com>
2026-04-16 16:01:08 -0400

    Admin dashboard nad Admin js
    Update admin dash, and admin js, functional and working correctly, not sure what else to add, could probably use more

--- src/main/resources/static/admindash/admin.js
+++ src/main/resources/static/admindash/admin.js
@@ -1,6 +1,54 @@
 /// Javascript for Admin Dashboard
 
+const adminCarUrl = new URL('http://localhost:8080/admin/getcars');
+const statsUrl = new URL('http://localhost:8080/admin/stats');
+const table = document.querySelector("#carTable tbody");
+const scrapeSummary = document.getElementById('scrapeSummary');
 
+//load the most recent 10 cars into the page. 
+
+function loadCars() {
+    table.innerHTML = "";
+    fetch(adminCarUrl)
+        .then(response => response.json())
+        .then(cars => {
+
+
+            cars.forEach(car => {
+                const row = `
+		                   <tr>
+		                       <td>${car.make}</td>
+		                       <td>${car.model}</td>
+		                       <td>${car.year}</td>
+		                       <td>$${car.price}</td>
+		                   </tr>
+		               `;
+                table.innerHTML += row;
+            });
+        });
+}
+
+
+function loadStats() {
+    //load stats ie, how much of each entity do we have. 
+    fetch(statsUrl)
+        .then(response => response.json())
+        .then(data => {
+            const userP = document.getElementById('userCount');
+            userP.textContent = data.userCount;
+            const carP = document.getElementById('carCount');
+            carP.textContent = data.carCount;
+            const favP = document.getElementById('favCount');
+            favP.textContent = data.favoriteCount;
+        })
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    loadCars();
+    loadStats();
+
+})
 
 
 //building timer
@@ -26,21 +74,53 @@ function updateCountdown() {
 setInterval(updateCountdown, 1000);
 updateCountdown();
 
+//function to update scrape info
+
+function loadScrapeData() {
+
+
+    fetch('http://localhost:8080/adminscrape')
+        .then(response => response.json()) // converts raw JSON to JS object/array
+        .then(data => {
+
+
+            //reload data and stats after every scrape
+            loadCars();
+            loadStats();
+            manualScrapeBtn.disabled = false;
+            manualScrapeBtn.textContent = "Trigger Scrape";
+
+            const date = new Date(data.completedDate);
+
+            const formattedDate = date.toLocaleString();
+
+            scrapeSummary.textContent = `Cars Added: ${data.carsAdded} | Time Allotted:
+						${data.timeTaken}s | Completed Date: ${formattedDate}`;
+
+        })
+
+}
+
+//constantly asking backend for updates, so the scheduling trigger is also displayed
+setInterval(() => {
+    loadScrapeData();
+}, 5000);
 
 const manualScrapeBtn = document.getElementById('manualTrigger');
 
-	manualScrapeBtn.addEventListener('click', function(){
-		
-		fetch('http://localhost:8080/adminscrape')
-		.then(response => response.json()) // converts raw JSON to JS object/array
-		        .then(data => {
-		
-					console.log(data);
-	})
-	
+manualScrapeBtn.addEventListener('click', function() {
 
-	
+    manualScrapeBtn.disabled = true;
+    manualScrapeBtn.textContent = "Scraping...";
+
+    loadScrapeData();
+
+
 })
+
+
+
+
 
 
 
